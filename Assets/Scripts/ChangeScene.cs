e791f9b7306f4d377d0c818aab7444ede4635bb4
Nikita Samarin <n.samarin.s@gmail.com>
2016-10-01 14:40:55 -0700

    Add Ethan

---
+++ Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+
+    void Start(){}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Portal"))
+        {
+            other.gameObject.SetActive(false);
+            SceneManager.LoadScene("Portal1Game", LoadSceneMode.Single);
+        }
+
+        if (other.gameObject.CompareTag("Portal 2"))
+        {
+            other.gameObject.SetActive(false);
+            SceneManager.LoadScene("Portal2Game", LoadSceneMode.Single);
+        }
+    }
+}
