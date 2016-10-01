85ef90e4d0591a797c96a07fc2b31c2ae392a51f
Nikita Samarin <n.samarin.s@gmail.com>
2016-10-01 08:25:20 -0700

    Add logic for player and camera movement

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    private Camera mainCamera;
+    private GameObject player;
+    private int viewMode;
+    private float cameraOffsetX;
+    private float cameraOffsetY;
+    private float cameraOffsetZ;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        mainCamera = GetComponent<Camera>();
+        mainCamera.transform.rotation = Quaternion.Euler(new Vector3(15f, 0, 0));
+
+        player = GameObject.Find("Player");
+        viewMode = 0;
+        updateCamera();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3 playerInfo = player.transform.transform.position;
+        mainCamera.transform.position = new Vector3(playerInfo.x + cameraOffsetX, playerInfo.y + cameraOffsetY, playerInfo.z + cameraOffsetZ);
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            float fYRot = Camera.main.transform.eulerAngles.y;
+            mainCamera.transform.rotation = Quaternion.Euler(new Vector3(15f, fYRot - 90.0f, 0));
+            if (viewMode == 0)
+            {
+                viewMode = 3;
+            }
+            else
+            {
+                viewMode--;
+            }
+            updateCamera();
+        }
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            float fYRot = Camera.main.transform.eulerAngles.y;
+            mainCamera.transform.rotation = Quaternion.Euler(new Vector3(15f, fYRot + 90.0f, 0));
+            if (viewMode == 3)
+            {
+                viewMode = 0;
+            }
+            else
+            {
+                viewMode++;
+            }
+            updateCamera();
+        }
+
+    }
+
+    public int getViewMode()
+    {
+        return viewMode;
+    }
+
+    void updateCamera()
+    {
+        switch (viewMode)
+        {
+            case 0:
+                cameraOffsetX = 0;
+                cameraOffsetY = 2.5f;
+                cameraOffsetZ = -7.0f;
+                break;
+            case 1:
+                cameraOffsetX = -7.0f;
+                cameraOffsetY = 2.5f;
+                cameraOffsetZ = 0;
+                break;
+            case 2:
+                cameraOffsetX = 0;
+                cameraOffsetY = 2.5f;
+                cameraOffsetZ = 7.0f;
+                break;
+            case 3:
+                cameraOffsetX = 7.0f;
+                cameraOffsetY = 2.5f;
+                cameraOffsetZ = 0;
+                break;
+            default:
+                Debug.LogError("INVALID CAMERA VIEW MODE!");
+                break;
+        }
+    }
+
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,20 +5,96 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody rb;
+    private float speed = 10.0F;
+    private float rotationSpeed = 20.0F;
+    public int viewMode;
+    private int directionX;
+    private int directionY;
+    private bool useHorizontal;
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        viewMode = 0;
+        updateControl();
     }
 
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        float moveHorizontal = directionX * Input.GetAxis("Horizontal") * rotationSpeed;
+        float moveVertical = directionY * Input.GetAxis("Vertical") * speed;
+        Vector3 movement;
+
+        if (useHorizontal)
+        {
+            movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        }
+        else
+        {
+            movement = new Vector3(moveVertical, 0.0f, moveHorizontal);
+        }
 
         rb.AddForce(movement);
 
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("Q!");
+
+            if (viewMode == 0)
+            {
+                viewMode = 3;
+            }
+            else
+            {
+                viewMode--;
+            }
+            updateControl();
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E!");
+            if (viewMode == 3)
+            {
+                viewMode = 0;
+            }
+            else
+            {
+                viewMode++;
+            }
+            updateControl();
+        }
+
+    }
+
+    void updateControl()
+    {
+        switch (viewMode)
+        {
+            case 0:
+                useHorizontal = true;
+                directionX = 1;
+                directionY = 1;
+                break;
+            case 1:
+                useHorizontal = false;
+                directionX = -1;
+                directionY = 1;
+                break;
+            case 2:
+                useHorizontal = true;
+                directionX = -1;
+                directionY = -1;
+                break;
+            case 3:
+                useHorizontal = false;
+                directionX = 1;
+                directionY = -1;
+                break;
+            default:
+                Debug.LogError("INVALID CAMERA VIEW MODE!");
+                break;
+        }
     }
 }
